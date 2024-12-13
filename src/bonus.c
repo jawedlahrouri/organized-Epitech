b80b99c65b11e6ffc0276da01d9dbc7ef37b9e03
jawed <jawed@epitech.eu>
2024-12-13 09:51:58 +0100

    fix: -h

--- src/bonus.c
+++ src/bonus.c
@@ -8,14 +8,14 @@
 int display_help(void)
 {
     my_printf("COMMANDS:\n");
-    my_printf("add: To use the 'add' command, write the type ");
+    my_printf("add : To use the 'add' command, write the type ");
     my_printf("in uppercase followed by the name of your object.\n");
-    my_printf("The only types supported are: ");
+    my_printf("      The only types supported are: ");
     my_printf("WIRE, DEVICE, SENSOR, PROCESSOR, and ACTUATOR.\n");
     my_printf("disp: Write 'disp' to display all your objects. ");
     my_printf("If you write anything else after 'disp', the ");
     my_printf("program will stop.\n");
-    my_printf("del: Write 'del' followed by <object id> ");
+    my_printf("del : Write 'del' followed by <object id> ");
     my_printf("to delete an object from your list.\n");
     my_printf("sort: The 'sort' command is not yet available.\n\n\n");
     my_printf("TIPS:\n");
